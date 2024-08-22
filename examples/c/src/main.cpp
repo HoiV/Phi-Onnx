c1ea5c87cb1659a4e7793884dbc937c38614a0bb
HoiV <36464824+HoiV@users.noreply.github.com>
2024-08-21 21:34:29 -0700

    Update main.cpp

--- examples/c/src/main.cpp
+++ examples/c/src/main.cpp
@@ -441,3 +441,65 @@ int main(int argc, char** argv) {
 
   return 0;
 }
+
+/*
+# Compiled library
+import onnxruntime_genai as engine
+
+# Option 1: Phi-3 mini 4K instruct model path
+model_path = "Phi-3-mini-4k-instruct-onnx/cpu_and_mobile/cpu-int4-rtn-block-32-acc-level-4"
+
+# Option 2: Uncomment the following line to use the Mistral 7B instruct v0.2 model path
+# model_path = "mistral-7b-instruct-v0.2-ONNX/onnx/cpu_and_mobile/mistral-7b-instruct-v0.2-cpu-int4-rtn-block-32-acc-level-4"
+
+# Option 3: Uncomment the following line to use the Phi-3 medium 128K instruct model path
+# model_path = "Phi-3-medium-128k-instruct-onnx-cpu/cpu-int4-rtn-block-32-acc-level-4"
+
+print(f"Loading {model_path} model\n")
+
+model = engine.Model(f'{model_path}')
+tokenizer = engine.Tokenizer(model)
+tokenizer_stream = tokenizer.create_stream()
+
+# Set the maximum number of tokens to generate
+# The full example includes additional parameters for more complex configurations
+search_options = {'max_length': 2048}
+
+# Template for formatting prompt messages in the script
+chat_tpl = '<|user|>\n{input}<|end|>\n<|assistant|>'
+
+print("Let's chat!\n")
+
+# Chat loop. Exit with Ctrl+C
+while True:
+    text = input("> User: ")
+    if not text:
+        print("Please, answer something")
+        continue
+
+    # Populate the chat template with user input
+    prompt = f'{chat_tpl.format(input=text)}'
+
+    input_tokens = tokenizer.encode(prompt)
+
+    gen_params = engine.GeneratorParams(model)
+    gen_params.set_search_options(**search_options)
+    gen_params.input_ids = input_tokens
+    generator = engine.Generator(model, gen_params)
+
+    print("\n> Assistant: ", end='', flush=True)
+
+    try:
+        # Loop to generate and display answer tokens
+        while not generator.is_done():
+            generator.compute_logits()
+            generator.generate_next_token()
+            next_token = generator.get_next_tokens()[0]
+            print(tokenizer_stream.decode(next_token), end='', flush=True)
+        print('\n')
+    except KeyboardInterrupt:
+        print("\nCtrl+C pressed, break\n")
+
+# Delete the generator
+del generator
+*/
